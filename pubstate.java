249b87efe4c14b970f1d6ac3f367385f7a3620a1
Samantha Helene C <91547802+IAmSamHeleneC@users.noreply.github.com>
2021-11-01 08:52:13 -0400

    File that was suppose to be pushed

---
+++ pubstate.java
@@ -0,0 +1,9 @@
+public class pubstate {
+    public static void main(String args[]){ 
+// Write a public static method called "toPower" that takes in as 
+// parameters two integers called size and power. 
+// The method should return an array of size "size" with each array index raised to the value of "power." 
+//  So, for example, if we passed in "size = 4" and "power = 2" to the "toPower," the method should return the following
+//     [0,1,4,9].
+    }
+}
