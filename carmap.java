62362c9fdfd010827c41bf3537a3159f96962700
Samantha Helene C <91547802+IAmSamHeleneC@users.noreply.github.com>
2021-11-01 08:49:16 -0400

    File That Was Suppose to Be Pushed

---
+++ carmap.java
@@ -0,0 +1,21 @@
+public class carmap {
+    public static void main(String args[]){ 
+    HashMap <String, Integer> CarInvent= new HashMap<>();
+
+    // “Hi, how can I help you?”
+    // Im looking for a affordable car.
+    // Here is what we have on display today,
+    
+CarInvent.put 'Honda', Civic, Elantra;
+CarInvent.put 'Nissan', Rogue, Altima; 
+CarInvent.put 'Dodge', Escape;
+CarInvent.put 'Toyata', Venza;
+
+        
+    // Any particular car you had in mind?
+    // I would like a Honda Civic.
+    // \"Oh, you're looking for a Civic? Our Honda selection is right over here...\"
+     System.out.println.containsKey (“Honda”));
+    
+}
+}
